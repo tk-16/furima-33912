34300155a5fab1e6d1fa737bb3143973e3f347ac
tk-16 <76659162+tk-16@users.noreply.github.com>
2021-01-15 18:07:54 +0900

    テストコードの追加・修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,15 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
+
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday])
   end
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER_FRIMA"] && password == ENV["BASIC_AUTH_PASSWORD_FRIMA"]  
+      username == ENV['BASIC_AUTH_USER_FRIMA'] && password == ENV['BASIC_AUTH_PASSWORD_FRIMA']
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,        presence: true
-  
+  validates :nickname, presence: true
+
+  with_options format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '半角英数字を使用してください' } do
+    validates :password
+  end
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
     validates :last_name
   end
-  
+
   with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角（カタカナ）を使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
-  
-  validates :birthday,        presence: true
+
+  validates :birthday, presence: true
 end
