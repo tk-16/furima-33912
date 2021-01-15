8835199c2ad0677593cfae9cefd4bb53103c12cd
tk-16 <76659162+tk-16@users.noreply.github.com>
2021-01-15 12:30:22 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,        presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name,      presence: true
+    validates :last_name,       presence: true
+  end
+  
+  with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角（カタカナ）を使用してください' } do
+    validates :first_name_kana, presence: true
+    validates :last_name_kana,  presence: true
+  end
+  
+  validates :birthday,        presence: true
 end
