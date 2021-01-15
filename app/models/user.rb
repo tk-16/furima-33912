d636c62c5eed3ca92f013e47a8a6cc729b3ed2a4
tk-16 <76659162+tk-16@users.noreply.github.com>
2021-01-15 17:01:04 +0900

    テストコードの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname,        presence: true
+  
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
-    validates :first_name,      presence: true
-    validates :last_name,       presence: true
+    validates :first_name
+    validates :last_name
   end
   
   with_options presence: true, format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角（カタカナ）を使用してください' } do
-    validates :first_name_kana, presence: true
-    validates :last_name_kana,  presence: true
+    validates :first_name_kana
+    validates :last_name_kana
   end
   
   validates :birthday,        presence: true
