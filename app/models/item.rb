16542583fa88430c7b1927cce916c8b8ba6c561b
tk-16 <76659162+tk-16@users.noreply.github.com>
2021-01-16 16:42:12 +0900

    Active_Hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,18 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one   :purchase, dependent: :destroy
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_area
+  belongs_to :delivery_day
+  belongs_to :delivery_fee
+
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :delivery_area_id, numericality: { other_than: 1 }
+  validates :delivery_day_id, numericality: { other_than: 1 }
+  validates :delivery_fee_id, numericality: { other_than: 1 } 
 end
