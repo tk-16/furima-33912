f46422619ac4304a12cc29145d6cd0a538aab643
tk-16 <76659162+tk-16@users.noreply.github.com>
2021-01-17 12:54:36 +0900

    販売手数料・販売利益の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,15 @@
+function post (){
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+   console.log(inputValue);
+   const tax =Math.floor(inputValue*0.1);
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = tax
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = Math.floor(inputValue - tax);
+  })
+
+  
+}
+window.addEventListener('load', post);
