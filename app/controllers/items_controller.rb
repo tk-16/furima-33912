c3f2beccef838f40c94835d35777dffd9ca9ebba
tk-16 <76659162+tk-16@users.noreply.github.com>
2021-01-18 21:27:53 +0900

    URL入力で編集ページへの遷移を制御

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new,:edit]
   def index
     @items = Item.order("created_at DESC")
   end
@@ -24,6 +24,10 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to action: :index
+    end
+    
   end
 
   def update
